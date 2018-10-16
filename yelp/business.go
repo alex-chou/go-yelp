4581d29632f114aeabe0191805b260f817a9a439
Alex Chou <achou@twitch.tv>
2018-10-15 21:30:08 -0700

    Move structs to top of the file (make it consistent).

--- yelp/business.go
+++ yelp/business.go
@@ -8,73 +8,10 @@ import (
 	"net/url"
 )
 
-// GetBusiness makes a request given the options provided.
-func (c *client) GetBusiness(ctx context.Context, gbo *GetBusinessOptions) (*Business, error) {
-	if err := gbo.Validate(); err != nil {
-		return nil, err
-	}
-	var respBody Business
-	_, err := c.authedDo(ctx, http.MethodGet, getBusinessPath(gbo), nil, nil, &respBody)
-	return &respBody, err
-}
-
-// getBusinessPath returns the business details path.
-func getBusinessPath(gbo *GetBusinessOptions) string {
-	return fmt.Sprintf("/v3/businesses/%s?%s", gbo.ID, gbo.URLValues().Encode())
-}
-
 // GetBusinessOptions contains the available parameters for the Get Business API.
 type GetBusinessOptions struct {
 	ID     string
 	Locale *string
-}
-
-// Validate returns an error with details when GetBusinessOptions are not valid.
-func (gbo *GetBusinessOptions) Validate() error {
-	switch {
-	case gbo == nil:
-		return errors.New("GetBusinessOptions are unset")
-	case gbo.ID == "":
-		return errors.New("GetBusinessOptions `ID` is not set")
-	case gbo.Locale != nil && ValidateLocale(*gbo.Locale) != nil:
-		return fmt.Errorf("GetBusinessOptions `Locale` is invalid: %s", *gbo.Locale)
-	default:
-		return nil
-	}
-}
-
-// URLValues returns GetBusinessOptions as url.Values.
-func (gbo *GetBusinessOptions) URLValues() url.Values {
-	if gbo == nil {
-		return nil
-	}
-
-	vals := url.Values{}
-	if gbo.Locale != nil {
-		vals.Add("locale", *gbo.Locale)
-	}
-	return vals
-}
-
-// Category describes a business.
-type Category struct {
-	Alias string `json:"alias"`
-	Title string `json:"title"`
-}
-
-// Hours has opening business hours for each day in a week.
-type Hours struct {
-	IsOpenNow bool   `json:"is_open_now"`
-	HoursType string `json:"hours_type"`
-	Open      []Open `json:"open"`
-}
-
-// Open has detailed opening business hours for each day in a week.
-type Open struct {
-	Day         int    `json:"day"`
-	Start       string `json:"start"`
-	End         string `json:"end"`
-	IsOvernight bool   `json:"is_overnight"`
 }
 
 // Business defines a business returned by the Yelp API.
@@ -103,4 +40,67 @@ type Business struct {
 
 	// The fields below are only available for Yelp Fusion VIP clients.
 	Attributes map[string]interface{} `json:"attributes"`
+}
+
+// Category describes a business.
+type Category struct {
+	Alias string `json:"alias"`
+	Title string `json:"title"`
+}
+
+// Hours has opening business hours for each day in a week.
+type Hours struct {
+	IsOpenNow bool   `json:"is_open_now"`
+	HoursType string `json:"hours_type"`
+	Open      []Open `json:"open"`
+}
+
+// Open has detailed opening business hours for each day in a week.
+type Open struct {
+	Day         int    `json:"day"`
+	Start       string `json:"start"`
+	End         string `json:"end"`
+	IsOvernight bool   `json:"is_overnight"`
+}
+
+// GetBusiness makes a request given the options provided.
+func (c *client) GetBusiness(ctx context.Context, gbo *GetBusinessOptions) (*Business, error) {
+	if err := gbo.Validate(); err != nil {
+		return nil, err
+	}
+	var respBody Business
+	_, err := c.authedDo(ctx, http.MethodGet, getBusinessPath(gbo), nil, nil, &respBody)
+	return &respBody, err
+}
+
+// getBusinessPath returns the business details path.
+func getBusinessPath(gbo *GetBusinessOptions) string {
+	return fmt.Sprintf("/v3/businesses/%s?%s", gbo.ID, gbo.URLValues().Encode())
+}
+
+// Validate returns an error with details when GetBusinessOptions are not valid.
+func (gbo *GetBusinessOptions) Validate() error {
+	switch {
+	case gbo == nil:
+		return errors.New("GetBusinessOptions are unset")
+	case gbo.ID == "":
+		return errors.New("GetBusinessOptions `ID` is not set")
+	case gbo.Locale != nil && ValidateLocale(*gbo.Locale) != nil:
+		return fmt.Errorf("GetBusinessOptions `Locale` is invalid: %s", *gbo.Locale)
+	default:
+		return nil
+	}
+}
+
+// URLValues returns GetBusinessOptions as url.Values.
+func (gbo *GetBusinessOptions) URLValues() url.Values {
+	if gbo == nil {
+		return nil
+	}
+
+	vals := url.Values{}
+	if gbo.Locale != nil {
+		vals.Add("locale", *gbo.Locale)
+	}
+	return vals
 }

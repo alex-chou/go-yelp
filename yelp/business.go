1ae787e36f40f8a72886ebc7e9ad9a88a046a1f4
Alex Chou <achou@twitch.tv>
2018-10-15 18:59:44 -0700

    Add in GetBusiness endpoint and add in updates to Business type.

--- yelp/business.go
+++ yelp/business.go
@@ -1,9 +1,65 @@
 package yelp
 
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+// GetBusiness makes a request given the options provided.
+func (c *client) GetBusiness(gbo *GetBusinessOptions) (*Business, error) {
+	if err := gbo.Validate(); err != nil {
+		return nil, nil
+	}
+	var respBody Business
+	_, err := c.authedDo(http.MethodGet, getBusinessPath(gbo), nil, nil, &respBody)
+	return &respBody, err
+}
+
+// getBusinessPath returns the business details path.
+func getBusinessPath(gbo *GetBusinessOptions) string {
+	return fmt.Sprintf("/v3/businesses/%s", gbo.ID)
+}
+
+// GetBusinessOptions contains the available parameters for the Get Business API.
+type GetBusinessOptions struct {
+	ID     string
+	Locale *string
+}
+
+// Validate returns an error with details when GetBusinessOptions is not valid.
+func (gbo *GetBusinessOptions) Validate() error {
+	switch {
+	case gbo.ID == "":
+		return errors.New("GetBusinessOptions `ID` is not set")
+	case gbo.Locale != nil:
+		if _, ok := ValidLocales[*gbo.Locale]; !ok {
+			return fmt.Errorf("GetBusinessOptions `Locale` is invalid: %s", *gbo.Locale)
+		}
+		// Note: make sure to check other conditions if locale is valid
+	}
+	return nil
+}
+
 // Category describes a business.
 type Category struct {
 	Alias string `json:"alias"`
 	Title string `json:"title"`
+}
+
+// Hours has opening business hours for each day in a week.
+type Hours struct {
+	IsOpenNow bool   `json:"is_open_now"`
+	HoursType string `json:"hours_type"`
+	Open      []Open `json:"open"`
+}
+
+// Open has detailed opening business hours for each day in a week.
+type Open struct {
+	Day         int    `json:"day"`
+	Start       string `json:"start"`
+	End         string `json:"end"`
+	IsOvernight bool   `json:"is_overnight"`
 }
 
 // Business defines a business returned by the Yelp API.
@@ -23,4 +79,13 @@ type Business struct {
 	ReviewCount  int64       `json:"review_count"`
 	URL          string      `json:"url"`
 	Transactions []string    `json:"transactions"`
+
+	// The fields below are only returned by the Get Business API.
+	Hours     []Hours  `json:"hours"`
+	Alias     *string  `json:"alias"`
+	IsClaimed *bool    `json:"is_claimed"`
+	Photos    []string `json:"photos"`
+
+	// The fields below are only available for Yelp Fusion VIP clients.
+	Attributes map[string]interface{} `json:"attributes"`
 }
